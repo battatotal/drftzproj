03a174298c21f5a185a374310d5b53997919610e
Al <battatotal@yandex.ru>
2023-06-29 18:35:51 +0300

    Создал эндпоинт регистрации нового участника(Профиль) 1)Добавил новые маршруты, в том числе для Логина 2)Создал собственный класс Permission 3)Добавилскрытое поле User в сериализатор Теперь, после регистрации пользователя через админку, можно создавать ему профиль через API. Профиль может быть создан только после авторизации,и не больше одного.

---
+++ drftzproj/people/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Profile
+
+
+class PeopleSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Profile
+        fields = '__all__'
